e083ced450f1b73a58836df91802484e1ec4f40d
Ahmed Shaaban <ahmed.shaaban.elhdah@gmail.com>
2024-07-26 11:06:17 +0300

    Add Api Router
    - Add CRUD operation
    - Inject store to api server
    - Implement the hander with store

--- types.go
+++ types.go
@@ -31,6 +31,6 @@ func NewAccount(firstName string, lastName string) *Account {
 		FirstName: firstName,
 		LastName:  lastName,
 		Number:    int64(rand.Intn(10000000)),
-		createdAt: time.Now().UTC(),
+		CreatedAt: time.Now().UTC(),
 	}
 }
